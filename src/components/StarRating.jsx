5de7ac9a35084a4d3ba1d9da5873d6e4654b13fe
Tutul Kabir <tutulkabir@gmail.com>
2024-02-22 11:24:27 +0530

    completed star rating projects

--- src/components/StarRating.jsx
+++ src/components/StarRating.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaStar } from "react-icons/fa";
+import "./style.css";
+const StarRating = ({ noOfStars = 5 }) => {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
 
-const StarRating = () => {
-  return <div>StarRating</div>;
+  const handleClick = (getCurrentIndex) => {
+    setRating(getCurrentIndex);
+  };
+  const handleMouseEnter = (getCurrentIndex) => {
+    setHover(getCurrentIndex);
+  };
+  const handleMouseLeave = () => {
+    setHover(rating);
+  };
+  return (
+    <div className="star-rating">
+      {[...Array(noOfStars)].map((_, index) => {
+        index += 1;
+        return (
+          <FaStar
+            key={index}
+            className={index <= (hover || rating) ? "active" : "inactive"}
+            onClick={() => handleClick(index)}
+            onMouseEnter={() => handleMouseEnter(index)}
+            onMouseLeave={() => handleMouseLeave()}
+            size={30}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default StarRating;
